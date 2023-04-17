73e99c2f33cb7000d77958ce4e13f55e7c7d7c9b
Julio C. Arroyo Ibarra <jarroyoi@login2.cm.cluster>
2023-04-16 17:34:43 -0700

    Can't save when submitting to SLURM.

--- preproc/save_images.py
+++ preproc/save_images.py
@@ -9,17 +9,22 @@ ID_COLUMN = "Unnamed: 0"
 PATH_COLUMN = "Path"
 DATA_PATH = "/groups/CS156b/data/"
 TRAIN_CSV_PATH = "/groups/CS156b/data/student_labels/train2023.csv"
-NUM_IMAGES = 10000
+NUM_IMAGES = 10
 
 
 if __name__ == "__main__":
     df = pd.read_csv(TRAIN_CSV_PATH)
     images = []
     for idx, row, in df.iterrows():
-        img = mpi.imread(DATA_PATH + row[PATH_COLUMN])
-        images.append(img)
+        if idx % 100 == 0:
+            print(f"Idx#{idx}")
         if idx >= NUM_IMAGES:
             break
+        img = mpi.imread(DATA_PATH + row[PATH_COLUMN])
+        images.append(img)
+
+    images = np.array(images)
+    print(f"JEFE: {images.shape}")
 
     with open("/groups/CS156b/2023/Xray-diagnosis/data/first10kimages.npy", "wb") as f:
-        np.save(f, np.array(images))
+        np.save(f, images)
