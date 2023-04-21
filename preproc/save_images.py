732b4fdae35febea3091ed6e80f7bf59432c86af
Julio C. Arroyo Ibarra <jarroyoi@login1.cm.cluster>
2023-04-20 21:24:56 -0700

    Implement image-saving script.

--- preproc/save_images.py
+++ preproc/save_images.py
@@ -1,4 +1,4 @@
-import matplotlib.image as mpi
+import cv2
 import pandas as pd
 import numpy as np
 
@@ -9,21 +9,22 @@ ID_COLUMN = "Unnamed: 0"
 PATH_COLUMN = "Path"
 DATA_PATH = "/groups/CS156b/data/"
 TRAIN_CSV_PATH = "/groups/CS156b/data/student_labels/train2023.csv"
-NUM_IMAGES = 100
-
+NUM_IMAGES = 3
+NEW_DIMS = 256
+NEW_DIMS = 256
 
 if __name__ == "__main__":
     print("BEGIN SCRIPT")
     df = pd.read_csv(TRAIN_CSV_PATH)
-    images = []
+    images = np.zeros((NUM_IMAGES, NEW_DIMS, NEW_DIMS), dtype=np.uint8)
     for idx, row, in df.iterrows():
         print(f"Idx#{idx}")
         if idx >= NUM_IMAGES:
             break
-        img = mpi.imread(DATA_PATH + row[PATH_COLUMN])
-        images.append(img)
+        img = cv2.imread(DATA_PATH + row[PATH_COLUMN], cv2.IMREAD_UNCHANGED)
+        img = cv2.resize(img, (NEW_DIMS, NEW_DIMS))
+        images[idx] = img
 
-    images = np.array(images)
     print(f"bytes1:")
     print(f"\tsize={images.size}")
     print(f"\titemsize={images.itemsize}")
