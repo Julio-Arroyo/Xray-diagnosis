97b131080a09a89a20d75ee7ca8972abb8e403d8
Julio Arroyo <jarroyoi@caltech.edu>
2023-04-30 18:04:45 -0700

    Add CNN definition.

---
+++ Cpp/include/scalarCNN.hpp
@@ -0,0 +1,24 @@
+#ifndef CNN_SCALAR_HPP
+#define CNN_SCALAR_HPP
+
+#include <torch/torch.h>
+
+class ScalarCNNImpl : public torch::nn::Module {
+public:
+    ScalarCNNImpl();
+    torch::Tensor forward(torch::Tensor x);
+
+private:
+    torch::nn::Conv2d conv1;
+    torch::nn::Conv2d conv2;
+    torch::nn::Conv2d conv3;
+
+    torch::nn::MaxPool2d pool1;
+    torch::nn::MaxPool2d pool2;
+
+    torch::nn::Linear fc1;
+    torch::nn::Linear fc2;
+};
+TORCH_MODULE(ScalarCNN);
+
+#endif
