80e0fe9723332c68f3afe94b19920ea245e30714
Julio Arroyo <jarroyoi@caltech.edu>
2023-04-30 23:16:39 -0700

    Validate in training loop.

--- Cpp/src/train.cpp
+++ Cpp/src/train.cpp
@@ -1,23 +1,32 @@
 #include "scalarCNN.hpp"
 #include "dataset.hpp"
 
-const std::string DataPath = "/groups/CS156b/2023/Xray-diagnosis/Cpp/data/PY_first60k.pt";
+const std::string TrainDataPath = "/groups/CS156b/2023/Xray-diagnosis/Cpp/data/PY_first60k_train.pt";
+const std::string ValDataPath = "/groups/CS156b/2023/Xray-diagnosis/Cpp/data/PY_first60k_val.pt";
 const int BatchSize = 64;
 const int NumEpochs = 10;
+const int TrainSetSize = 47998;
+const int ValSetSize = 11999;
 
 int main() {
     torch::Device device(torch::kCUDA);
     std::cout << "Got device: " << device << std::endl;
 
-    auto dataset = CheXpert(DataPath).map(torch::data::transforms::Stack<>());
+    auto train_dataset = CheXpert(TrainDataPath).map(torch::data::transforms::Stack<>());
+    auto val_dataset = CheXpert(ValDataPath).map(torch::data::transforms::Stack<>());
+    std::cout << "Dataset sizes train/val: " << train_dataset.size().value() << ", " << val_dataset.size().value() << std::endl;
 
     ScalarCNN model;
 
-    auto data_loader = torch::data::make_data_loader(std::move(dataset),
-                                            torch::data::DataLoaderOptions()
-                                                        .batch_size(BatchSize)
-                                                        .workers(2));
-
+    auto train_loader = torch::data::make_data_loader(std::move(train_dataset),
+                                                      torch::data::DataLoaderOptions()
+                                                                  .batch_size(BatchSize)
+                                                                  .workers(2));
+    auto val_loader = torch::data::make_data_loader(std::move(val_dataset),
+                                                    torch::data::DataLoaderOptions()
+                                                                .batch_size(ValSetSize)
+                                                                .workers(2));
+  
     torch::optim::Adam optim(model->parameters(), torch::optim::AdamOptions(2e-4)
                                                     .betas(std::make_tuple(0.5, 0.5)));
 
@@ -26,8 +35,9 @@ int main() {
     // Todo restore from checkpoint
 
     for (int epoch = 1; epoch <= NumEpochs; ++epoch) {
+        model->train();
         double running_loss = 0.0;
-        for (torch::data::Example<>& batch : *data_loader) {
+        for (torch::data::Example<>& batch : *train_loader) {
             torch::Tensor inputs = batch.data.to(device);
             torch::Tensor labels = batch.target.to(device);
 
@@ -43,10 +53,25 @@ int main() {
             optim.step();
         }
 
+        model->eval();
+        double val_loss = 0.0;
+        for (torch::data::Example<>& batch : *val_loader) {
+            torch::Tensor inputs = batch.data.to(device);
+            torch::Tensor labels = batch.target.to(device);
+
+            torch::Tensor preds = model->forward(inputs);
+
+            torch::nn::functional::MSELossFuncOptions MSEoptions(torch::kSum);
+            auto loss = torch::nn::functional::mse_loss(preds, labels, MSEoptions);
+            double curr_val_loss = loss.item<double>();
+            val_loss += curr_val_loss;
+        }
+
         std::printf(
-            "\r[Epoch: %2ld/%2ld] Loss: %.4f",
+            "\r[Epoch: %2ld/%2ld] Train Loss: %.4f",
             epoch,
             NumEpochs,
-            running_loss/59997.0);
+            running_loss/TrainSetSize,
+            val_loss/ValSetSize);
     }
 }
