9a7fbb0d603c9d94790bfdbf84c53bf21c811ad6
Julio Arroyo <jarroyoi@caltech.edu>
2023-05-04 13:45:47 -0700

    Calculate 3-way split.

--- preproc/save_images.py
+++ preproc/save_images.py
@@ -3,19 +3,25 @@ import pandas as pd
 import numpy as np
 
 
+# TOTAL IMAGES = 178158
+# SPLIT INTO THREE PART OF SIZES: 60000, 59079, 59079
+
 ROWNUM_COLUMN = "Unnamed: 0.1"
 ID_COLUMN = "Unnamed: 0"
 PATH_COLUMN = "Path"
 DATA_PATH = "/groups/CS156b/data/"
 TRAIN_CSV_PATH = "/groups/CS156b/data/student_labels/train2023.csv"
-NUM_IMAGES = 50000
-START = 10000
-NEW_DIMS = 256
-NEW_DIMS = 256
+NUM_IMAGES = 60000
+START = 0
+NEW_DIMS = 224
+
 
 if __name__ == "__main__":
     print("BEGIN SCRIPT")
     df = pd.read_csv(TRAIN_CSV_PATH)
+    NUM_IMAGES = len(df.index) - START
+    print(f"NUM_IMAGES: {NUM_IMAGES}")
+    assert NUM_IMAGES > 0
     images = np.zeros((NUM_IMAGES, NEW_DIMS, NEW_DIMS), dtype=np.uint8)
     for idx, row, in df.iterrows():
         print(f"Idx#{idx}")
@@ -35,5 +41,5 @@ if __name__ == "__main__":
     
     print(f"JEFE: {images.shape}")
 
-    with open(f"/groups/CS156b/2023/Xray-diagnosis/data/10k_to_60k.npy", "wb") as f:
+    with open(f"/groups/CS156b/2023/Xray-diagnosis/data/part1_allTrain_224x224.npy", "wb") as f:
         np.save(f, images)
