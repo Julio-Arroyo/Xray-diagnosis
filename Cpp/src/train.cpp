00f7f4bbe6d4a1ca4e95424604f204d99ae8389a
Julio Arroyo <jarroyoi@caltech.edu>
2023-05-25 01:04:15 -0700

    Implement validation metrics: top-1 accuracy, MSE.

--- Cpp/src/train.cpp
+++ Cpp/src/train.cpp
@@ -11,7 +11,6 @@ const int BatchSize = 128;
 const int NumEpochs = 10;
 const int NumResidualBlocksPerStack = 9;
 const bool SkipConnections = true;
-const int LoggingInterval = 100;  // record loss every LoggingInterval iterations
 const int NumWorkers = 4;
 
 
@@ -28,7 +27,7 @@ int main(int argc, char *argv[]) {
 
     std::ofstream loss_history_file;
     loss_history_file.open("../logs/LOSS_" + ExperimentName + ".txt");
-    loss_history_file << "TrainLoss,ValLoss,ValAcc\n";
+    loss_history_file << "TrainLoss,ValLoss,ValAcc,ValMSE\n";
 
     // Resnet18
     std::cout << "NumResidualBlocksPerStack: " << NumResidualBlocksPerStack << std::endl;
@@ -46,9 +45,13 @@ int main(int argc, char *argv[]) {
 
     auto train_dataset = CheXpert(TrainDataPath).map(torch::data::transforms::Stack<>());
     auto val_dataset = CheXpert(ValDataPath).map(torch::data::transforms::Stack<>());
+
     const int TrainSetSize = train_dataset.size().value();
+    const int NumTrainBatches = TrainSetSize/BatchSize + 1;
     const int ValSetSize = val_dataset.size().value();
+    const int NumValBatches = ValSetSize/BatchSize + 1;
     std::cout << "Dataset sizes train/val: " << TrainSetSize << ", " << ValSetSize << std::endl;
+    std::cout << "Number batches train/val: " << NumTrainBatches << ", " << NumValBatches << std::endl;
 
     auto train_loader = torch::data::make_data_loader(std::move(train_dataset),
                                                       torch::data::DataLoaderOptions()
@@ -65,14 +68,14 @@ int main(int argc, char *argv[]) {
     model->to(device);
 
     auto criterion = torch::nn::CrossEntropyLoss();
-    // torch::nn::functional::MSELossFuncOptions MSEoptions(torch::kMean);
+    torch::nn::MSELossOptions MSEoptions(torch::kMean);
+    auto MSEcriterion = torch::nn::MSELoss(MSEoptions);
 
     // Todo restore from checkpoint
 
     double train_loss = 0;
     double best_val_acc = 0;
     for (int epoch = 1; epoch <= NumEpochs; ++epoch) {
-        int debug_num_train_batches = 0;  // DEBUG
         for (torch::data::Example<>& train_batch : *train_loader) {            
             model->train();
             torch::Tensor train_inputs = train_batch.data.to(torch::kFloat32);
@@ -89,15 +92,14 @@ int main(int argc, char *argv[]) {
             optim.zero_grad();
             curr_train_loss.backward();
             optim.step();
-            debug_num_train_batches++;
         }
 
         // torch::InferenceMode guard(true);  // DEBUG
         torch::NoGradGuard no_grad;
         model->eval();
         double val_loss = 0;
+        double val_mse = 0;
         int val_num_correct = 0;
-        int num_val_batches = 0;  // DEBUG
         for (torch::data::Example<>& val_batch : *val_loader) {
             torch::Tensor val_inputs = val_batch.data.to(torch::kFloat32);
             torch::Tensor val_labels = val_batch.target.to(torch::kFloat32);
@@ -117,24 +119,25 @@ int main(int argc, char *argv[]) {
 
             val_num_correct += val_preds.eq(val_classes).sum().item<int64_t>();
 
-            auto v_loss = criterion(val_output, val_labels);
-            val_loss += v_loss.item<double>();
-            num_val_batches++;
+            auto curr_val_loss = criterion(val_output, val_labels);
+            auto curr_val_mse = MSEcriterion(val_preds, val_classes);
+            val_loss += curr_val_loss.item<double>();
+            val_mse += curr_val_mse.item<double>();
         }
 
-        std::cout << "DEBUG predicted num train batches: " << TrainSetSize / BatchSize << ". Actual: " << debug_num_train_batches << std::endl;
-        std::cout << "DEBUG predicted num val batches: " << ValSetSize / BatchSize << ". Actual: " << num_val_batches << std::endl;
-        train_loss = train_loss / debug_num_train_batches;  // DEBUG
-        val_loss = val_loss / num_val_batches;
+        train_loss = train_loss / NumTrainBatches;
+        val_loss = val_loss / NumValBatches;
+        val_mse = val_mse / NumValBatches;
         double val_accuracy = static_cast<double>(val_num_correct) / ValSetSize;
 
         std::printf(
-            "\r[Epoch: %2ld] Train Loss: %.8f | Val Loss: %.8f | Val Acc: %.8f\n",
+            "\r[Epoch: %2ld] Train Loss: %.8f | Val Loss: %.8f | Val Acc: %.8f | Val MSE: %.8f\n",
             epoch,
             train_loss,
             val_loss,
-            val_accuracy);
-        loss_history_file << train_loss << "," << val_loss << "," << val_accuracy << "\n";
+            val_accuracy,
+            val_mse);
+        loss_history_file << train_loss << "," << val_loss << "," << val_accuracy << "," << val_mse << "\n";
 
         if (val_accuracy > best_val_acc) {
             best_val_acc = val_accuracy;
